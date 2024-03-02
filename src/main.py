4edb327ab142735b3b85dc93fea794b8b10faae8
novah451 <jzamora0451@gmail.com>
2024-03-01 18:25:34 -0800

    added description to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+# In the future, this program will be the first program the user runs
+# It will: spawn a server/room, connect user to their created room/allow user
+# to join whichever room is available w/ access code
 
+# Some features mentioned may be put into seperate files
+# (e.g., the creation of a room may be done with spawn.py)
+
+print("Hello, and welcome to Novah's Simple TCP Chatroom!")
