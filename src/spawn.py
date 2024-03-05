bce8fe513cb4d86da66078c57aacd14b11ec1de7
novah451 <jzamora0451@gmail.com>
2024-03-04 23:53:18 -0800

    desc for spawn

---
+++ src/spawn.py
@@ -0,0 +1,6 @@
+# TODO: create a function that spawns a terminal with server.py running
+# This will allow for any room to become active one a user requests to
+# either create one or join one. This probably makes no sense
+# but in my head it does, so that's what counts
+
+print("This will spawn a terminal with server.py running on a specific port\n");
